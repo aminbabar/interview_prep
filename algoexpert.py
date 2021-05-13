6e2822c6e838dc1379677c9e09a8b90a6dc7d662
Amin <amin.babar1@gmail.com>
2021-05-12 22:50:07 -0400

    work needs to be done on question 3

--- algoexpert.py
+++ algoexpert.py
@@ -1,66 +1,116 @@
-#Interview Prep
+# #Interview Prep
 
 
-# Question 1
+# # Question 1
+# ################################################################################
+# # Brute force method
+# # 0(n^2) time. O(1) space
+
+# def twoNumberSum1(array, targetSum):
+#     for i in array:
+#         for j in array:
+#             if i == j:
+#                 continue
+#             if i + j == targetSum:
+#                 return [i, j]
+
+#     return []
+
+
+# # If we value time complexity over space complexity
+# # O(n) time. O(n) space
+# def twoNumberSum2(array, targetSum):
+#     myDict = {}
+#     for num in array: 
+#         if (targetSum - num) in myDict:
+#             return [num, targetSum - num]
+#         else:
+#             myDict[num] = True
+#     return []
+
+
+
+# # If we value space complexity more than time complexity:
+
+# # O(nlog(n)) time complexity
+# # O(1) space complexity
+# def twoNumberSum3(array, targetSum):
+#     array.sort()
+
+#     #left pointer and right pointer
+#     left = 0
+#     right = len(array) - 1
+#     while left != right:
+#         if array[left] + array[right] == targetSum:
+#             return [array[left], array[right]]
+
+#         elif array[left] + array[right] > targetSum:
+#             right -= 1
+
+#         else: # array[left] + array[right] < targetSum:
+#             left += 1
+#     return []
+
+# ################################################################################
+
+
+# # NOTE: O(n long(n)) > O(n)
+
+
+
+# # Question 2
+# ################################################################################
+
+
+
+# # O(n) runtime 
+# def isValidSubsequence(array, sequence):
+#     if sequence.len() > array.len():
+#         return False
+    
+#     for num in array: 
+#         if sequence.len() == 0:
+#             return True
+#         elif num == sequence[0]:
+#             sequence.pop(0)
+        
+#     return False
+
+# ################################################################################
+
+
+
+# Question 3
 ################################################################################
-# Brute force method
-# 0(n^2) time. O(1) space
-
-def twoNumberSum1(array, targetSum):
-    for i in array:
-        for j in array:
-            if i == j:
-                continue
-            if i + j == targetSum:
-                return [i, j]
-
-    return []
-
-
-# Smart method
-# O(n) time. O(n) space
-def twoNumberSum2(array, targetSum):
-    myDict = {}
-    for num in array: 
-        if (targetSum - num) in myDict:
-            return [num, targetSum - num]
-        else:
-            myDict[num] = True
-    return []
-
-
-
-# Smartest method
-# O(nlog(n)) time complexity
-# O(1) space complexity
-def twoNumberSum3(array, targetSum):
-    array.sort()
-
-    #left pointer and right pointer
+def sortedSquaredArray(array):
+    # Write your code here.
+    if len(array) == 1:
+        return [array[0] ** 2]
+    
     left = 0
     right = len(array) - 1
-    while left != right:
-        if array[left] + array[right] == targetSum:
-            return [array[left], array[right]]
-
-        elif array[left] + array[right] > targetSum:
+    
+    sqArr = []
+    
+    while True:
+        print(left)
+        print(right)
+        leftVal = array[left] ** 2
+        rightVal = array[right] ** 2
+        if left == right:
+            sqArr.append(leftVal)
+            break
+        if leftVal > rightVal:
+            sqArr.append(leftVal)
+            left += 1
+        else:
+            sqArr.append(rightVal)
             right -= 1
 
-        else: # array[left] + array[right] < targetSum:
-            left += 1
-    return []
+    sqArr.reverse()
+    return sqArr
+        
 
-################################################################################
+array = [-1, 2]
 
-
-
-# Question 2
-################################################################################
-
-
-
-
-
-
-
-
+print(sortedSquaredArray(array))
